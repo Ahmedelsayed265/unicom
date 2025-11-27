e82597c3327e3974bbc26c4e1e5fbda908264c03
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-11-27 22:07:13 +0200

    first

---
+++ src/lib/axiosApi.ts
@@ -0,0 +1,42 @@
+import axios, { type AxiosRequestConfig } from "axios";
+
+export const axiosApi = axios.create({
+  baseURL: "http://45.94.209.137:100/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const getRequest = async <T>(
+  url: string,
+  config?: AxiosRequestConfig
+): Promise<T> => {
+  const res = await axiosApi.get<T>(url, config);
+  return res.data;
+};
+
+export const postRequest = async <T>(
+  url: string,
+  data?: Record<string, unknown>,
+  config?: AxiosRequestConfig
+): Promise<T> => {
+  const res = await axiosApi.post<T>(url, data, config);
+  return res.data;
+};
+
+export const putRequest = async <T>(
+  url: string,
+  data?: Record<string, unknown>,
+  config?: AxiosRequestConfig
+): Promise<T> => {
+  const res = await axiosApi.put<T>(url, data, config);
+  return res.data;
+};
+
+export const deleteRequest = async <T>(
+  url: string,
+  config?: AxiosRequestConfig
+): Promise<T> => {
+  const res = await axiosApi.delete<T>(url, config);
+  return res.data;
+};
---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <App />
+  </QueryClientProvider>
+);
